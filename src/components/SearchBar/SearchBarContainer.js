35d3ac2ed199b726e1ca197b978604173bdba639
Janneth Williams <47696793+Jwilliams85@users.noreply.github.com>
2020-05-24 23:41:25 -0400

    changes

--- src/components/SearchBar/SearchBarContainer.js
+++ src/components/SearchBar/SearchBarContainer.js
@@ -1,35 +1,39 @@
 // You do not need to change any code in this file
 import React from "react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faInstagram } from '@fortawesome/free-brands-svg-icons'
-import { faHeart, faCircle, faCompass } from '@fortawesome/free-regular-svg-icons'
 import "./SearchBar.css";
 
 const SearchBar = () => {
-  return (
-    <div className="search-bar-wrapper">
-      <div className="social">
-        <FontAwesomeIcon icon={faInstagram} />
-      </div>
-      <form className="search-form">
-        <input
-          type="text"
-          placeholder="Search"
-        />
-      </form>
-      <div className="social-wrapper">
-        <div className="social">
-          <FontAwesomeIcon icon={faCompass} />
-        </div>
-        <div className="social">
-          <FontAwesomeIcon icon={faHeart} />
-        </div>
-        <div className="social">
-          <FontAwesomeIcon icon={faCircle} />
-        </div>
-      </div>
-    </div>
-  );
+return (
+
+<div className="search-bar-wrapper">
+<div className="image-wrapper">
+<i className="fab fa-instagram" />
+</div>
+<form className="search-form">
+<input
+type="text"
+placeholder="Search"
+/>
+</form>
+<div className="social-wrapper">
+<div className="social">
+<i className="far fa-compass" />
+</div>
+<div className="social">
+<i className="far fa-heart" />
+</div>
+<div className="social">
+<i className="far fa-user-circle" />
+</div>
+</div>
+</div>
+);
 };
 
 export default SearchBar;
+
+
+
+
+
+
